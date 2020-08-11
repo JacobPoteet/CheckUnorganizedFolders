6dadc824a3daacd8421c03a3077532f86014ee88
JacobPoteet <64765148+gwlpotassium@users.noreply.github.com>
2020-08-10 23:55:56 -0500

    Initial commit

---
+++ CheckForUnorganized.h
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <fstream>
+//#include <filesystem>
+
+
+using namespace std;
+//namespace fs = std::filesystem;
+
+//------------------------------------
+/*Global Variable*/
+//-----------------------------------
+string FileToSearch = "FoldersToCheck.jp";
+ifstream folderFile;
+
+//------------------------------------
+
+
+
+
+
+//------------------------------------
+/*Functions*/
+//------------------------------------
+
+//keep command prompt active
+void PauseCommandPrompt() {system("pause");};
+
+
+// Returns all file paths that need checked
+vector<string> LoadInPaths();
+
+//------------------------------------
---
+++ README.md
@@ -0,0 +1,2 @@
+# CheckUnorganizedFolders
+ Find and describe all specified folders for any includes files. This is used to check various "ToBeOrganized" folders that might be the target for OBS outputs.
